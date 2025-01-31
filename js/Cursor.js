6b49e9b83b38e38ce86719349aa8a57300bda91c
Stevesuk <stevesukawa@outlook.com>
2025-01-31 17:29:48 +0800

    ++++

--- js/Cursor.js
+++ js/Cursor.js
@@ -18,25 +18,23 @@ document.addEventListener('mousemove', (e) => {
     customCursor.style.top = `${e.clientY}px`;
 
     const element = document.elementFromPoint(e.clientX, e.clientY);
-    
+    const rect = element.getBoundingClientRect();
+    // JavaScript 部分
     if (
         element &&
         element !== document.documentElement &&
         element !== customCursor &&
-        !element.classList.contains('container') &&
-        !element.classList.contains('welcomeOnMotd') &&
-        !element.classList.contains('motd') &&
-        !element.classList.contains('desc') &&
-        !element.classList.contains('links')
-    ) {
-        const rect = element.getBoundingClientRect();
-        customCursor.style.width = `${rect.width + 30}px`;
+        element.classList.contains('hyperlink')
+    ) {    
+        customCursor.style.backgroundColor = 'rgb(185, 0, 255)'; 
+        customCursor.style.width = `${rect.width + 15}px`;
         customCursor.style.height = `${rect.height + 3}px`;
         customCursor.style.left = `${(rect.right + rect.left) / 2}px`;
         customCursor.style.top = `${(rect.bottom + rect.top) / 2}px`;
-        customCursor.style.borderRadius = '10px';
+        customCursor.style.borderRadius = '5px';
         customCursor.style.opacity = '0.5';
-    } else {
+    } else {     
+        customCursor.style.backgroundColor = 'rgb(255, 0, 175)'; 
         customCursor.style.width = '35px';
         customCursor.style.height = '35px';
         customCursor.style.borderRadius = '50%';
