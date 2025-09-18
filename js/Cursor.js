a5313b74d9f2fdb9f4aef85dd8ad7b98a80bdf1d
Stevesuk <stevesukawa@outlook.com>
2025-09-18 14:15:11 +0900

    more animations

--- js/Cursor.js
+++ js/Cursor.js
@@ -3,29 +3,6 @@ c.id = 'o';
 document.body.append(c);
 
 const s = document.createElement('style');
-s.textContent = `
-  #o {
-    pointer-events: none;
-    position: absolute;
-    width: 30px;
-    height: 30px;
-    background: #66cdaa;
-    backdrop-filter: blur(8px) saturate(180%);
-    border-radius: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 9999999999;
-    border: 2px solid #fff;
-    transition:
-      opacity 0.3s ease,
-      transform 0.3s ease,
-      width 0.25s,
-      height 0.5s,
-      left 0.05s,
-      top 0.05s,
-      backdrop-filter 0.5s,
-      background-color 0.2s;
-  }
-`;
 document.head.append(s);
 
 let timeout, animationFrame;
@@ -44,30 +21,36 @@ document.addEventListener('mousemove', e => {
     c.style.transform = 'translate(-50%, -50%) scale(1)';
     c.style.left = e.clientX + 'px';
     c.style.top = e.clientY + 'px';
+    c.style.position = 'absolute';
+    c.style.pointerEvents = 'none';
+    c.style.width = '30px';
+    c.style.height = '30px';
+    c.style.backgroundColor = '#66cdaa';
+    c.style.backdropFilter = 'blur(8px) saturate(180%)';
+    c.style.borderRadius = '50%';
+    c.style.zIndex = '9999999999';
+    c.style.border = '2px solid #fff';
+    c.style.transition = 'opacity 0.3s ease, transform 0.3s ease, width 0.25s, height 0.5s, left 0.05s, top 0.05s, backdrop-filter 0.5s, background-color 0.2s';
 
     const hoveredElement = document.elementFromPoint(e.x, e.y);
     const linkRect = hoveredElement?.closest('a')?.getBoundingClientRect();
 
     if (linkRect) {
-      Object.assign(c.style, {
-        zIndex: -9999,
-        background: rgb(102, 205, 170),
-        width: linkRect.width + 15 + 'px',
-        height: linkRect.height + 5 + 'px',
-        left: (linkRect.left + linkRect.right) / 2 + 'px',
-        top: (linkRect.top + linkRect.bottom) / 2 + 'px',
-        borderRadius: '5px',
-        opacity: '0.3'
-      });
+      c.style.zIndex = '-9999';
+      c.style.backgroundColor = 'rgb(143, 1, 251)';
+      c.style.width = linkRect.width + 15 + 'px';
+      c.style.height = linkRect.height + 5 + 'px';
+      c.style.left = (linkRect.left + linkRect.right) / 2 + 'px';
+      c.style.top = (linkRect.top + linkRect.bottom) / 2 + 'px';
+      c.style.borderRadius = '5px';
+      c.style.opacity = '0.3';
     } else {
-      Object.assign(c.style, {
-        zIndex: 9999,
-        background: rgb(52, 205, 120),
-        width: '35px',
-        height: '35px',
-        borderRadius: '50%',
-        opacity: '0.9'
-      });
+      c.style.zIndex = '9999';
+      c.style.backgroundColor = 'rgb(255, 0, 175)';
+      c.style.width = '35px';
+      c.style.height = '35px';
+      c.style.borderRadius = '50%';
+      c.style.opacity = '0.9';
     }
   });
 });
